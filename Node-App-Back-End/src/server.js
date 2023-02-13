daf0d401aead7a71a9c5a31aeb6f4059cb0f9ab4
RafiAthallahN <rafiraikaza@gmail.com>
2023-02-14 05:56:28 +0700

    tes

--- Node-App-Back-End/src/server.js
+++ Node-App-Back-End/src/server.js
@@ -2,7 +2,7 @@ const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
-    port: 5000,
+    port: 5001,
     host: 'localhost',
   });
   server.route(routes)
